90d7aaaa008bde6d621559aa816b4ca8b79dafed
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-07-25 09:00:51 +0930

    Initial Commit

---
+++ Landmarks-/Landmarks_App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_App.swift
+//  Landmarks-
+//
+//  Created by Matthew Morrison on 25/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
