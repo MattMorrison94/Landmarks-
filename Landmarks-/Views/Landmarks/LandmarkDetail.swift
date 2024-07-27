1e3f1a7daccac2dd23e015dbbf131e45c2973815
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-07-27 11:15:33 +0930

    stage 4 - handling user input

--- Landmarks-/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks-/Views/Landmarks/LandmarkDetail.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
 
 
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
+
     var body: some View {
+        @Bindable var modelData = modelData
+
+
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -23,8 +33,11 @@ struct LandmarkDetail: View {
 
 
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
 
 
                 HStack {
@@ -50,6 +63,9 @@ struct LandmarkDetail: View {
     }
 }
 
+
 #Preview {
-    LandmarkDetail(landmark: landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
