9bd7bf671ab2995b50e6cec1b72a9e6062dde39e
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-07-28 11:40:13 +0930

    Added  - isFeatured to Landmakr  - Computed features to ModelData  - Category List  - TabView to contentview

--- Landmarks-/Views/ContentView.swift
+++ Landmarks-/Views/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTab: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        VStack {
+        TabView {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star.fill")
+                }
+                .tag(Tab.featured)
+            
             LandmarksList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
         }
     }
 }
